3a3d08bbe379c7c8c4d6ac0553848ee53831a6c8
Schmoney <114448874+schmoney@users.noreply.github.com>
2022-12-24 04:05:44 -0500

    sketch gen complete

--- run.js
+++ run.js
@@ -1,21 +1,28 @@
 //etch-a-sketch
+//Odin Project
 
 const content = document.querySelector('.content');
-const grid = document.querySelector('.grid');
 
 function gridGen(){
     let gridSize = prompt("size?");
+
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
+    content.appendChild(grid);
 
     for(i = 1; i <= (gridSize*gridSize); i++){
         let gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
         gridItem.classList.add(`grid-item-${i}`);
-        gridItem.style.cssText = `width: ${500/gridSize}px;`;
 
         grid.appendChild(gridItem);
     }
 }
 
-
-
 gridGen();
+
+const gridItem = document.querySelectorAll('.grid-item');
+gridItem.forEach((gridItem) => gridItem.addEventListener('mouseover', function() {
+    gridItem.style.backgroundColor = 'red';
+}));
